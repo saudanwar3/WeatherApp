07fdb27d829ff36d3bbc4dcd15e4d54ee03a8bb2
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2022-05-23 00:32:58 +0400

    Added weather detail view controller, modified launch screen

---
+++ WeatherApp/ViewControllers/WeatherDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetailViewController.swift
+//  WeatherApp
+//
+//  Created by Muhammad Saud Anwar on 23/05/2022.
+//
+
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
